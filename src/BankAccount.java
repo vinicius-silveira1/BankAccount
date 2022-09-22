0ab838822f6ebaca566b815105c88e8105c42801
Vinícius Silveira <vinifs19@gmail.com>
2022-09-22 19:40:00 -0300

    Added tests

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -7,4 +7,38 @@ public class BankAccount {
         this.balance = 0;
         this.isOpen = false;
     }
+
+    public void open() {
+        this.isOpen = true;
+    }
+
+    public void close() {
+        this.isOpen = false;
+    }
+
+    
+
+    
+    private boolean withdrawBiggerThanBalance(double value) {
+        if (this.balance < value) {
+            System.out.println("Cannot withdraw more money than is currently in the account");
+            return true;
+        }
+    }
+    private boolean accountEmpty() {
+        if (this.balance == 0) {
+            System.out.println("Cannot withdraw money from an empty account");
+            return true;
+        }
+    }
+    private boolean negativeValue(double value) {
+        if (value < 0) {
+            System.out.println("Cannot deposit or withdraw negative amount");
+            return true;
+        }
+    }
+    
 }
+
+    
+
