0cfc12930c6e7da9d2ef5af64198f4c20aa8e6a8
Vinícius Silveira <vinifs19@gmail.com>
2022-09-22 19:12:08 -0300

    Added constructor and protected fields

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,5 +1,10 @@
 public class BankAccount {
+    
+    protected double balance;
+    protected boolean isOpen;
+    
     public BankAccount() {
-        
+        this.balance = 0;
+        this.isOpen = false;
     }
 }
